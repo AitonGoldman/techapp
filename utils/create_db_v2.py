b15c63151152cf9bc94624dcf8b807d604d76a23
Aiton Goldman <agoldma@us.ibm.com>
2018-07-24 07:01:10 -0400

    create db

---
+++ utils/create_db_v2.py
@@ -0,0 +1,30 @@
+from sqlalchemy_utils import create_database, database_exists, drop_database as sql_utils_drop_database
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from lib.TableProxy import TableProxy
+import os,sys
+
+if len(sys.argv) > 1:
+    db_name=sys.argv[1]    
+else:
+    print "didn't specify db name..."
+    sys.exit(1)
+
+
+db_username=os.environ['db_username']
+db_password=os.environ['db_password']
+db_name='techapp'
+    
+db_url="postgresql://%s:%s@localhost/%s" % (db_username,db_password,db_name)    
+
+flask_app = Flask('dummy')
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+
+    
+if database_exists(db_url):        
+    sql_utils_drop_database(db_url)
+create_database(db_url)
+db_handle = SQLAlchemy(flask_app)
+table_proxy = TableProxy(db_handle)
+db_handle.create_all()            
+
